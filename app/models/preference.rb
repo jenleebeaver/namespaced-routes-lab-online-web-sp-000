28aa390cbfcbd55d791f3f14f52220c137f1c100
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-07 20:16:13 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,6 +1,6 @@
 
 class Preference < ActiveRecord::Base
-  #applicationrecord allows us to use this as a single point of entry by creating this model - we would have to create this other model:
+  # applicationrecord allows us to use this as a single point of entry by creating this model - we would have to create this other model:
   # class ApplicationRecord < ActiveRecord::Base
   # self.abstract_class = true
   # end
